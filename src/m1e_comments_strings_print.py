6f3717503c184467da0f14b43f36a7016406196b
baileykc <baileykc@rose-hulman.edu>
2019-01-17 09:38:12 -0500

    Example

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -22,7 +22,7 @@
 ###############################################################################
 
 
-print('Hello, World')
+print('Hello, Killian')
 print('hi there')
 print('one', 'two', 'buckle my shoe')
 
